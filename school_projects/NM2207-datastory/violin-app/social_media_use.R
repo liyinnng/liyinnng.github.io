7d7c977bfb4444b1067e1bf36a883a2b6fe3dd76
Li Ying <sohliying02@gmail.com>
2023-11-15 13:49:20 +0800

    heatmap updates

--- school_projects/NM2207-datastory/violin-app/social_media_use.R
+++ school_projects/NM2207-datastory/violin-app/social_media_use.R
@@ -1,8 +1,8 @@
 library(shiny)
-library(ggplot2)
+library(plotly)
 library(dplyr)
-library(rlang)
 
+# Read data
 social_media_health <- read.csv("social-media-and-mental-health.csv")
 
 # Filter age outliers
@@ -19,25 +19,21 @@ ui <- fluidPage(
         label = "Choose a variable to analyze:",
         choices = c(
           "Frequency of facing issues regarding sleep",
-          "Difficulty in concentrating on things"
+          "Difficulty in concentrating on things",
+          "Being bothered by worries"
         ),
         selected = "Frequency of facing issues regarding sleep"
       )
     ),
     mainPanel(
-      plotOutput("plot")
+      plotlyOutput("plot")
     )
   )
 )
 
 # Define server
 server <- function(input, output) {
-  label_mapping <- list(
-    "X20..On.a.scale.of.1.to.5..how.often.do.you.face.issues.regarding.sleep." = "Frequency of Sleep Issues",
-    "X14..Do.you.find.it.difficult.to.concentrate.on.things." = "Difficulty in Concentrating on Things"
-  )
-  
-  create_plot <- function(variable) {
+  create_heatmap <- function(variable) {
     data <- social_media_health
     title <- ""
     y_label <- ""
@@ -48,31 +44,55 @@ server <- function(input, output) {
     } else if (variable == "Difficulty in concentrating on things") {
       title <- "Comparison of Concentration Ability"
       y_label <- "X14..Do.you.find.it.difficult.to.concentrate.on.things."
+    } else if (variable == "Being bothered by worries") {
+      title <- "Comparison of Worries Botheration"
+      y_label <- "X13..On.a.scale.of.1.to.5..how.much.are.you.bothered.by.worries."
     }
     
     if (title == "" || y_label == "") {
       return(NULL)
     }
     
-    gg <- ggplot(data, aes(x = X6..Do.you.use.social.media., y = !!sym(y_label))) +
-      geom_violin(aes(fill = X6..Do.you.use.social.media.), scale = "width") +
-      labs(
-        title = title,
-        caption = "Note: 1 being no difficulty faced, and 5 being very difficult",
-        x = "Do you use social media?",
-        y = label_mapping[[y_label]], 
-        fill = "Do you use social media"
-      ) +
-      scale_fill_manual(values = c("No" = "#D64045", "Yes" = "#9FC0DE"))
+    heatmap_data <- table(data$X6..Do.you.use.social.media., data[[y_label]])
     
-    return(gg)
+    if (sum(heatmap_data) == 0) {
+      # Handle case where the heatmap data is empty
+      return(NULL)
+    }
+    
+    # Transpose the data matrix
+    heatmap_data_transposed <- t(heatmap_data)
+    
+    # Create custom hover text
+    hover_text <- matrix(
+      paste("Frequency: ", heatmap_data_transposed),
+      nrow = nrow(heatmap_data_transposed),
+      ncol = ncol(heatmap_data_transposed),
+      byrow = TRUE
+    )
+    
+    heatmap <- plot_ly(
+      x = colnames(heatmap_data_transposed),
+      y = rownames(heatmap_data_transposed),
+      z = as.matrix(heatmap_data_transposed),
+      type = "heatmap",
+      colorscale = list(c(0,1), c("#FFFFFF", "#FACA5E")),
+      colorbar = list(title = "Frequency")
+    ) %>% 
+      layout(
+        title = title,
+        xaxis = list(title = "Do you use social media?"),
+        yaxis = list(title = y_label)
+      )
+    
+    return(heatmap)
   }
   
-  output$plot <- renderPlot({
+  output$plot <- renderPlotly({
     variable <- input$variable
-    plot <- create_plot(variable)
-    if (!is.null(plot)) {
-      print(plot)
+    heatmap <- create_heatmap(variable)
+    if (!is.null(heatmap)) {
+      heatmap
     }
   })
 }
