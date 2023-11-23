a65b74853592dc481670d7906567f5634e46f90f
Li Ying <sohliying02@gmail.com>
2023-11-23 21:21:24 +0800

    updates to data story telling writing

--- school_projects/NM2207-datastory/bargraph-app/app.R
+++ school_projects/NM2207-datastory/bargraph-app/app.R
@@ -1,68 +1,92 @@
 library(shiny)
 library(echarts4r)
 library(dplyr)
+library(tidyr)
+library(magrittr)
 
 # Load your dataset
 social_media_health <- read.csv("social-media-and-mental-health.csv")
 
-# Define UI for dataset viewer app ----
+social_media_health <- mutate(social_media_health,
+                              age_group = case_when(
+                                X1..What.is.your.age. <= 16 ~ "Child",
+                                between(X1..What.is.your.age., 17, 30) ~ "Youth",
+                                between(X1..What.is.your.age., 31, 45) ~ "Adults",
+                                X1..What.is.your.age. > 45 ~ "Old-aged adults",
+                                TRUE ~ NA_character_
+                              ))
+
+# Function to filter data based on selected gender
+filter_data <- function(data, agegroup) {
+  if (agegroup == "All ages") {
+    return(data)
+  } else {
+    return(filter(data, age_group == agegroup))
+  }
+}
+
+# Define UI
 ui <- fluidPage(
   titlePanel("Social Media and Mental Health"),
   sidebarLayout(
     sidebarPanel(
-      # selector for age group
       selectInput(
-        inputId = "select_gender",
-        label = "Select Gender",
-        choices = c("All", "Female", "Male"),
-        selected = "All"
+        inputId = "select_agegroup", 
+        label = "Select Age Group", 
+        choices = c("All ages", "Child", "Youth", "Adults", "Old-aged adults"),
+        selected = "All ages"
       )
     ),
     mainPanel(
-      echarts4rOutput("bar_plot1"),
-      echarts4rOutput("bar_plot2"),
-      height = "1000px"
+      echarts4rOutput("myChart")
     )
   )
 )
 
-# Define the server logic
+# Define server
 server <- function(input, output) {
-  # Create a reactive expression for filtered data
+  # Reactive function to filter data based on selected gender
   filtered_data <- reactive({
-    if (input$select_gender == "All") {
-      return(social_media_health)
-    } else {
-      return(social_media_health[social_media_health$X2..Gender == input$select_gender, ])
-    }
+    filter_data(social_media_health, input$select_agegroup)
   })
   
-  # Create two echarts4r bar plots
-  output$bar_plot1 <- renderEcharts4r({
-    filtered <- filtered_data()
-    counts <- table(filtered$X15..On.a.scale.of.1.5..how.often.do.you.compare.yourself.to.other.successful.people.through.the.use.of.social.media.)
-    bar_data <- data.frame(Comparison = names(counts), Count = as.numeric(counts))
+  observe({
+    df_X15 <- filtered_data() %>%
+      group_by(X15..On.a.scale.of.1.5..how.often.do.you.compare.yourself.to.other.successful.people.through.the.use.of.social.media.) %>%
+      summarise(count_X15 = n())
     
-    echarts4r() %>%
-      e_bar(Comparison, Count) %>%
-      e_title("Comparison Frequency") %>%
-      e_x_axis(name = "How often do you compare yourself to other successful people through SM?") %>%
-      e_y_axis(name = "Count")
-  })
-  
-  output$bar_plot2 <- renderEcharts4r({
-    filtered <- filtered_data()
-    counts <- table(filtered$X16..Following.the.previous.question..how.do.you.feel.about.these.comparisons..generally.speaking.)
-    bar_data <- data.frame(Feelings = names(counts), Count = as.numeric(counts))
+    df_X16 <- filtered_data() %>%
+      group_by(X16..Following.the.previous.question..how.do.you.feel.about.these.comparisons..generally.speaking.) %>%
+      summarise(count_X16 = n())
     
-    echarts4r() %>%
-      e_bar(Feelings, Count) %>%
-      e_title("Feelings") %>%
-      e_x_axis(name = "What are your feelings following the previous question?") %>%
-      e_y_axis(name = "Count")
+    # Merge the two data frames by their respective columns
+    df <- full_join(df_X15, df_X16, by = c(
+      "X15..On.a.scale.of.1.5..how.often.do.you.compare.yourself.to.other.successful.people.through.the.use.of.social.media." = "X16..Following.the.previous.question..how.do.you.feel.about.these.comparisons..generally.speaking."
+    ))
+    
+    # Calculate the minimum non-missing value for X15
+    min_X15 <- ifelse(all(is.na(df$X15..On.a.scale.of.1.5..how.often.do.you.compare.yourself.to.other.successful.people.through.the.use.of.social.media.)),
+                      0, min(df$X15..On.a.scale.of.1.5..how.often.do.you.compare.yourself.to.other.successful.people.through.the.use.of.social.media., na.rm = TRUE))
+    
+    output$myChart <- renderEcharts4r({
+      df %>%
+        e_charts(
+          `X15..On.a.scale.of.1.5..how.often.do.you.compare.yourself.to.other.successful.people.through.the.use.of.social.media.`,
+          axisPointer = list(
+            link = list(
+              xAxisIndex = "all"
+            )
+          )
+        ) %>%
+        e_x_axis(min = min_X15) %>%
+        e_line(count_X15, name = "comparison frequency") %>%
+        e_area(count_X16, name = "comparison feelings") %>%
+        e_tooltip(formatter = htmlwidgets::JS("function(params) {return params.seriesName + ': ' + params.value[1];}")) %>%
+        e_theme("macarons2")
+    })
   })
 }
 
-# Create Shiny app ----
-shinyApp(ui = ui, server = server)
+# Create Shiny app
+shinyApp(ui, server)
 
